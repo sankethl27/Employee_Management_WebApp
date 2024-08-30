bfe70a92985c70eb300e5a1a6d802d6996101292
=sankethl27 <sankethlreddy@gmail.com>
2024-08-30 11:46:45 +0530

    commit1-

---
+++ src/test/java/com/Sanketh/Employee_Management/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sanketh.Employee_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
