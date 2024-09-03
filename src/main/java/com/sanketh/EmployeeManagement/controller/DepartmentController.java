8fd19997ec10f1f8d3a8e59afa376fba6bff2f4a
=sankethl27 <sankethlreddy@gmail.com>
2024-09-03 22:13:54 +0530

    Security added

--- src/main/java/com/sanketh/EmployeeManagement/controller/DepartmentController.java
+++ src/main/java/com/sanketh/EmployeeManagement/controller/DepartmentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@RequestMapping("/api")
+
 @RestController
 public class DepartmentController {
 
---
+++ src/main/java/com/sanketh/EmployeeManagement/controller/RootController.java
@@ -0,0 +1,22 @@
+package com.sanketh.EmployeeManagement.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @Autowired
+    DepartmentController departmentController;
+
+    @GetMapping("/")
+    public String homePage(){
+        return "home";
+    }
+    @GetMapping("/login")
+    public String loginController(){
+        return "login";
+    }
+}
